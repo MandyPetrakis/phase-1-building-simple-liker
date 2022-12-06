5753c5c503ccb3936f5cbd70ebf7dbb541a96f02
Mandy Petrakis <mandypetrakis@gmail.com>
2022-12-06 11:14:13 -0800

    commit tree 00f456f645614caae6ede89a2a3c8ab9f49bb5c3

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
 
+let heart = document.querySelector(".like-glyph");
+heart.forEach((like) =>
+  like.addEventListener("click", () => console.log("clicked"))
+);
 
-
+// iterate over the hearts array
+// call the function, save it to a variable, use then on the variable
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
